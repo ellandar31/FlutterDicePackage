72ad440634bd1a3f1db7c0d5ef4b26139d3f63f3
Ellandar_L <barre.ludovic@gmail.com>
2024-10-14 15:11:36 +0000

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,28 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:start/guard/profile.dart';
 import 'package:start/src/home.dart';
-import 'package:start/utilitaires/decorations.dart';
 
-import 'firebase_options.dart';
-import 'guard/auth_gate.dart';
-
-final actionCodeSettings = ActionCodeSettings(
-  url: 'https://gamingportal-1345c.web.app/',
-  handleCodeInApp: true,
-  androidMinimumVersion: '1',
-  androidPackageName: 'io.flutter.plugins.firebase_ui.firebase_ui_example',
-  iOSBundleId: 'io.flutter.plugins.fireabaseUiExample',
-);
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
-  String get initialRoute {
-    final user = FirebaseAuth.instance.currentUser;
-
-    if (user == null) {
-      return '/';
-    }
-    return '/home';
-  }
-  
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final mfaAction = AuthStateChangeAction<MFARequired>(
-      (context, state) async {
-        final nav = Navigator.of(context);
-        await startMFAVerification(
-          resolver: state.resolver,
-          context: context,
-        );
-        nav.pushReplacementNamed('/home');
-      },
-    );
-
     return ChangeNotifierProvider(
-      create: (BuildContext context) {  },
+      create: (context) => MyAppState(),
       child: MaterialApp(
-          title: 'Gaming Portal',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-          ),
-          initialRoute: initialRoute,
-          routes: {
-            '/': (context) {
-              return AuthGate();
-            },
-            '/forgot-password': (context) {
-              final arguments = ModalRoute.of(context)?.settings.arguments
-                  as Map<String, dynamic>?;
-
-              return ForgotPasswordScreen(
-                email: arguments?['email'],
-                headerMaxExtent: 200,
-                headerBuilder: headerIcon(Icons.lock),
-                sideBuilder: sideIcon(Icons.lock),
-              );
-            },           
-            '/profile': (context) {
-              return ProfilePage(true);
-            },        
-            '/home': (context) {
-              return HomeScreen();
-            },
-          }),
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: HomePage(),
+      ),
     );
   }
 }
 
+class MyAppState extends ChangeNotifier {}
--- lib/src/dicelib/dice_roller.dart
+++ lib/src/dicelib/dice_roller.dart
@@ -1,21 +1,21 @@
-
 import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:start/src/dicelib/svg_dices.dart';
+import './svg_dices.dart';
 
 class DiceRoller extends StatefulWidget {
   final Color diceColor;
   final bool displayBtn;
-  const DiceRoller({super.key, required this.diceColor,  this.displayBtn = true});
+  const DiceRoller(
+      {super.key, required this.diceColor, this.displayBtn = true});
 
   @override
-  State<DiceRoller> createState() => _DiceRollerState();
+  State<DiceRoller> createState() => DiceRollerState();
 }
 
-class _DiceRollerState extends State<DiceRoller> {
+class DiceRollerState extends State<DiceRoller> {
   Random random = Random();
   int currentImageIndex = 0;
   double currentRotation = 0;
@@ -48,11 +48,11 @@ class _DiceRollerState extends State<DiceRoller> {
             angle: currentRotation * 180,
             child: SvgPicture.string(
               imagesTxt[currentImageIndex],
-              height: 100,
+              height: 60,
             ),
           ),
           const SizedBox(height: 10),
-          if(displayBtn) rollButton(),
+          if (displayBtn) rollButton(),
         ],
       ),
     );
@@ -60,32 +60,32 @@ class _DiceRollerState extends State<DiceRoller> {
 
   ElevatedButton rollButton() {
     return ElevatedButton(
-          style: ElevatedButton.styleFrom(backgroundColor:buttonBGColor),
-          onPressed: () async {
-            // Rolling the dice
-            _launchRoll();
-          },
-          child: const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(
-              'Roll',
-              style: TextStyle(fontSize: 26, color:Colors.black),
-            ),
-          ),
-        );
+      style: ElevatedButton.styleFrom(backgroundColor: buttonBGColor),
+      onPressed: () async {
+        // Rolling the dice
+        launchRoll();
+      },
+      child: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text(
+          'Roll',
+          style: TextStyle(fontSize: 26, color: Colors.black),
+        ),
+      ),
+    );
   }
 
-  void _launchRoll() async{
+  void launchRoll() async {
     Timer.periodic(const Duration(milliseconds: 80), (timer) {
       counter++;
       setState(() {
         currentImageIndex = random.nextInt(6);
         currentRotation = random.nextDouble();
       });
-    
+
       if (counter >= 13) {
         timer.cancel();
-    
+
         setState(() {
           currentRotation = 0;
           counter = 1;
